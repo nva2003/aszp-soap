0ef609cbd99140e794550c6e92ce9a820c2a1dbc
user <nva2003yandex.ru>
2020-09-16 15:13:47 +0300

    Change IRI

--- src/main/java/rzd/pktbcdt/aszp/soap/ws/WSEndpoint.java
+++ src/main/java/rzd/pktbcdt/aszp/soap/ws/WSEndpoint.java
@@ -2,6 +2,6 @@ package rzd.pktbcdt.aszp.soap.ws;
 
 public interface WSEndpoint {
     String NAMESPACE_URI = "http://aszp.pktbcdt.rzd/ProjectsService";
-    String WS_URI = "/ws";
+    String WS_URI = "/aszp-soap/ws";
     String WSDL = "projects";
 }
--- src/main/java/rzd/pktbcdt/aszp/soap/ws/config/WebServiceConfig.java
+++ src/main/java/rzd/pktbcdt/aszp/soap/ws/config/WebServiceConfig.java
@@ -24,7 +24,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(servlet, "/ws/*");
+        return new ServletRegistrationBean(servlet, "/aszp-soap/ws/*");
     }
 
     @Bean(name = "projects")
