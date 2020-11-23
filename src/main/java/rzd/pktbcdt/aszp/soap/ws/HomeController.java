234d47ad8d7b2374b24f023afeac057b69c1bde2
nva <nva@yandex.ru>
2020-11-23 17:16:21 +0300

    add homepage

--- src/main/java/rzd/pktbcdt/aszp/soap/ws/HomeController.java
+++ src/main/java/rzd/pktbcdt/aszp/soap/ws/HomeController.java
@@ -1,11 +1,16 @@
 package rzd.pktbcdt.aszp.soap.ws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Properties;
 
 /**
  * User: vnikishin
@@ -31,25 +36,42 @@ public class HomeController implements Serializable {
 for slf4j
 private static final Logger logger = LoggerFactory.getLogger( SimpleController.class );
 */
-    
+
     /*--------------------------------------------
     |    I N S T A N C E   V A R I A B L E S    |
     ============================================*/
     @Value("${spring.application.name}")
     String appName;
 
+    String version = "UNDEFINED";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     /*--------------------------------------------
     |         C O N S T R U C T O R S           |
     ============================================*/
-    
-    
+
+
     /*--------------------------------------------
     |               M E T H O D S               |
     ============================================*/
     @GetMapping("/aszp-soap")
-//    @GetMapping("/aszp-soap")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+//        prop.load(getServletContext().getResourceAsStream("/META-INF/MANIFEST.MF"));
+        Properties prop = new Properties();
+        try {
+            prop.load( is );
+            version = prop.getProperty("Implementation-Build");
+            if (version ==null) {
+                version = this.getClass().getPackage().getImplementationVersion();
+            }
+        } catch (IOException ex) {
+            version = this.getClass().getPackage().getImplementationVersion();
+            logger.error(ex.getLocalizedMessage());
+        }
+
+        model.addAttribute("version", version);
         return "home";
     }
 }
--- src/main/java/rzd/pktbcdt/aszp/soap/ws/config/MvcConfig.java
+++ src/main/java/rzd/pktbcdt/aszp/soap/ws/config/MvcConfig.java
@@ -1,14 +1,16 @@
 package rzd.pktbcdt.aszp.soap.ws.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@EnableWebMvc
+//@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    public void addViewControllers(ViewControllerRegistry registry) {
+/*    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-    }
+    }*/
 }
