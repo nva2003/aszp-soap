6ecacda80c803698005d2619aeeafe07f2334f77
nva <nva@yandex.ru>
2021-05-25 10:17:40 +0300

    add IP Address of client  logging

--- src/main/java/rzd/pktbcdt/aszp/soap/ws/ProjectsServiceEndpoint.java
+++ src/main/java/rzd/pktbcdt/aszp/soap/ws/ProjectsServiceEndpoint.java
@@ -7,7 +7,12 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
+import org.springframework.ws.transport.http.HttpServletConnection;
 import rzd.pktbcdt.aszp.soap.ws.model.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Endpoint
 public class ProjectsServiceEndpoint implements WSEndpoint{
@@ -23,6 +28,8 @@ public class ProjectsServiceEndpoint implements WSEndpoint{
 
         logger.debug("request = " + request.toString());
 
+        logger.info("The IP Address of client is : " + getClientIpAddr(this.getHttpServletRequest()));
+
         return projectsService.getASZPProjects(request);
 
     }
@@ -32,6 +39,8 @@ public class ProjectsServiceEndpoint implements WSEndpoint{
     public GetProjectsResponse getProjects(@RequestPayload GetProjectsRequest request) {
 
         logger.debug("request = " + request.toString());
+
+        logger.info("The IP Address of client is : " + getClientIpAddr(this.getHttpServletRequest()));
 
         GetProjectsResponse response = new GetProjectsResponse();
         response.setProjects(projectsService.getProjects(request));
@@ -45,6 +54,7 @@ public class ProjectsServiceEndpoint implements WSEndpoint{
 
         logger.debug("request = " + request.toString());
 
+        logger.info("The IP Address of client is : " + getClientIpAddr(this.getHttpServletRequest()));
         return projectsService.getProjectInfo(request);
 
     }
@@ -55,7 +65,54 @@ public class ProjectsServiceEndpoint implements WSEndpoint{
 
         logger.debug("request = " + request.toString());
 
+        logger.info("The IP Address of client is : " + getClientIpAddr(this.getHttpServletRequest()));
+
         return projectsService.getProjectTree(request);
 
+    }
+
+    protected HttpServletRequest getHttpServletRequest(){
+        TransportContext transportContext = TransportContextHolder.getTransportContext();
+        HttpServletConnection httpServletConnection = (HttpServletConnection) transportContext.getConnection();
+        HttpServletRequest request = httpServletConnection.getHttpServletRequest();
+        return request ;
+    }
+
+    private static String getClientIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_VIA");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
